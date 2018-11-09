2bef1558950f49be290504d0774c0001b7c6faa8
Ignacio Cabrera <cabrerabywaters@gmail.com>
2018-11-09 16:18:00 +0100

    fix redefine model

--- src/Fluent/Fluent.js
+++ src/Fluent/Fluent.js
@@ -1,15 +1,16 @@
-import stampit from '@stamp/it';
-import compose from '@stamp/compose';
-import Model from './Model';
-import Collection from './Collection';
+import stampit from "@stamp/it";
+import compose from "@stamp/compose";
+import Model from "./Model";
+import Collection from "./Collection";
 
-var _FLUENT_REMOTE_CONNECTORS, _FLUENT_LOCAL_CONNECTORS, _FLUENT_MERGE_CONNECTORS
+var _FLUENT_REMOTE_CONNECTORS,
+  _FLUENT_LOCAL_CONNECTORS,
+  _FLUENT_MERGE_CONNECTORS;
 const Fluent = stampit({
   init() {
     this.registerGlobalVariable();
   },
-  properties: {
-  },
+  properties: {},
   methods: {
     model(...args) {
       this.registerModel(args);
@@ -24,34 +25,43 @@ const Fluent = stampit({
       return Model.compose(...args);
     },
     collect(args) {
-      return Collection(args)
+      return Collection(args);
     },
     registerGlobalVariable() {
-      if ((typeof window !== 'undefined') && window) {
-        window._FLUENT_ = { ...window._FLUENT_, ...{ models: {} } };
+      if (typeof window !== "undefined" && window) {
+        window._FLUENT_ = { ...window._FLUENT_ };
       }
 
       if (global) {
-        global._FLUENT_ = { ...global._FLUENT_, ...{ models: {} } };
+        global._FLUENT_ = { ...global._FLUENT_ };
       }
     },
     registerModel(args) {
       let name =
-        args && args[0] && args[0].properties && args[0].properties.name ?
-          args[0].properties.name : undefined
+        args && args[0] && args[0].properties && args[0].properties.name
+          ? args[0].properties.name
+          : undefined;
 
       if (!name) {
         return;
       }
 
-      if (!(typeof name === 'string')) {
+      if (!(typeof name === "string")) {
         throw new Error(
-          'You must assign a name to your Model when using Fluent.compose'
+          "You must assign a name to your Model when using Fluent.compose"
         );
       }
 
-      if (name !== 'baseModel') {
-        if (typeof window !== 'undefined') {
+      if (window && window._FLUENT_ && !window._FLUENT_.models) {
+        window._FLUENT_.models = {};
+      }
+
+      if (global && global._FLUENT_ && !global._FLUENT_.models) {
+        global._FLUENT_.models = {};
+      }
+
+      if (name !== "baseModel") {
+        if (typeof window !== "undefined") {
           window._FLUENT_.models[name] = true;
         }
         global._FLUENT_.models[name] = true;
@@ -62,37 +72,33 @@ const Fluent = stampit({
       LOCAL_CONNECTORS = undefined,
       MERGE_CONNECTORS = undefined
     }) {
-      if ((typeof window !== 'undefined') && window) {
-        window._FLUENT_ = {
-          ...window._FLUENT_, ...{
-            connectors: {
-              local: LOCAL_CONNECTORS,
-              remote: REMOTE_CONNECTORS,
-              merge: MERGE_CONNECTORS
-            }
-          }
-        };
+      if (typeof window !== "undefined" && window) {
+        if (!window._FLUENT_.connectors) {
+          window._FLUENT_.connectors = {
+            local: LOCAL_CONNECTORS,
+            remote: REMOTE_CONNECTORS,
+            merge: MERGE_CONNECTORS
+          };
+        }
       }
 
-      if ((typeof global !== 'undefined') && global) {
-        global._FLUENT_ = {
-          ...global._FLUENT_, ...{
-            connectors: {
-              local: LOCAL_CONNECTORS,
-              remote: REMOTE_CONNECTORS,
-              merge: MERGE_CONNECTORS
-            }
-          }
-        };
+      if (typeof global !== "undefined" && global) {
+        if (!global._FLUENT_.connectors) {
+          global._FLUENT_.connectors = {
+            local: LOCAL_CONNECTORS,
+            remote: REMOTE_CONNECTORS,
+            merge: MERGE_CONNECTORS
+          };
+        }
       }
     },
     getConfig() {
-      if ((typeof window !== 'undefined') && window) {
-        return window._FLUENT_
+      if (typeof window !== "undefined" && window) {
+        return window._FLUENT_;
       }
 
-      if ((typeof global !== 'undefined') && global) {
-        return global._FLUENT_
+      if (typeof global !== "undefined" && global) {
+        return global._FLUENT_;
       }
     }
   }
